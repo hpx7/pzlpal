18f2504f108777abd13a046e034aa33e014b0c2b
Siddharth Dhulipalla <dsiddharth@gmail.com>
2014-02-01 04:31:15 -0500

    fucked shit

--- algo.js
+++ algo.js
@@ -1,17 +1,69 @@
-var data = require("./data.js");
+var data = require("./good-data.js");
 var slots = data.array;
+var maxSlots;
+var maxBoard;
+var maxSlotsCount = 0;
+
+for (var i = 0; i < slots.length; i++) {
+    slots[i].startx--;
+    slots[i].starty--;
+}
+
+function isEmpty(ans, replaceStar) {
+  ans = ans.replace(/\s+/g, '');
+  if (replaceStar)
+    ans = ans.replace(/\*+/g, '');
+  return ans === '';
+}
+
+function updateBest(board) {
+  var fullSlotsCount = 0;
+  for (var i = 0; i < slots.length; i++)
+    if (!isEmpty(slots[i].answer, true))
+      fullSlotsCount++;
+
+  if (fullSlotsCount > maxSlotsCount) {
+    maxSlotsCount = fullSlotsCount;
+    maxSlots = clone(slots);
+    maxBoard = clone(board);
+  }
+}
 
 function clone (obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
-function getSlotIndex () {
+function getSlotIndex (board) {
+  var maxCollisions = -1;
+  var mcSlotIndex = -1;
+
   for (var i = 0; i < slots.length; i++) {
     var slot = slots[i];
-    if (slot.answer === "")
-      return i;
+    if (!isEmpty(slot.answer, false))
+      continue;
+
+    collisions = 0;
+    for (var j = 0; j < slot.length; j++) {
+      var curX = slot.startx;
+      var curY = slot.starty;
+      if (slot.orientation == "across")
+          curX += j;
+      else
+          curY += j;
+
+      var curC = board[curY][curX];
+      // if curC is a real char and is different from what we want to insert
+      if (curC !== null)
+        collisions++;
+    }
+
+    if (collisions > maxCollisions) {
+      maxCollisions = collisions;
+      mcSlotIndex = i;
+    }
   }
-  return -1;
+
+  return mcSlotIndex;
 }
 
 function getPossibleAnswers (slotIndex) {
@@ -20,8 +72,6 @@ function getPossibleAnswers (slotIndex) {
 
 function canInsert(board, slotIndex, possibleAnswerIdx) {
   var slot = slots[slotIndex];
-  // console.log("slotIndex" + slotIndex);
-  // console.log(slot);
   var possibleAnswer = slot.answers[possibleAnswerIdx];
   for (var i = 0; i < possibleAnswer.name.length; i++) {
       var curX = slot.startx;
@@ -33,8 +83,7 @@ function canInsert(board, slotIndex, possibleAnswerIdx) {
 
       var curC = board[curY][curX];
       // if curC is a real char and is different from what we want to insert
-      // XXX test undefined
-      if (curC !== undefined && curC != possibleAnswer.name[i])
+      if (curC !== null && curC != possibleAnswer.name[i])
         return false;
   }
   return true;
@@ -53,7 +102,7 @@ function insert (board, slotIndex, possibleAnswerIdx) {
           curY += i;
       newBoard[curY][curX] = possibleAnswer.name[i];
   }
-  slots[slotIndex].answer = possibleAnswer;
+  slots[slotIndex].answer = possibleAnswer.name;
   return newBoard;
 }
 
@@ -67,16 +116,29 @@ function fillInAnswer (board, slotIndex, possibleAnswerIdx) {
 }
 
 function finished () {
-  return getSlotIndex() === -1;
+  return maxSlotsCount === slots.length;
+  // for (var i = 0; i < slots.length; i++) {
+  //   var slot = slots[i];
+  //   if (slot.answer.replace(/\s+/g, '') !== '')
+  //     return false;
+  // }
+  // console.log("finished");
+  // return true;
 }
 
 function eraseAnswer (slotIndex) {
-  slots[slotIndex].answer = "";
+  var newStr = "";
+  for (var i = 0; i < slots[slotIndex].length; i++) 
+    newStr += " ";
+  slots[slotIndex].answer = newStr;
 }
 
-solve = function (board) {
-	console.log(board)
-  var slotIndex = getSlotIndex();
+solve = function (board, numSkips) {
+	// printBoard(board);
+  var slotIndex = getSlotIndex(board);
+  if (slotIndex === -1) {
+    return false;
+  }
   var possibleAnswers = getPossibleAnswers(slotIndex);
   for (var possibleAnswerIdx = 0; possibleAnswerIdx < possibleAnswers.length; possibleAnswerIdx++) {
     var newBoard = fillInAnswer(board, slotIndex, possibleAnswerIdx);
@@ -87,19 +149,62 @@ solve = function (board) {
     if (finished())
       return true;
 
-    var solved = solve(newBoard);
+    updateBest(board);
+    var solved = solve(newBoard, numSkips);
     if (solved)
       return true;
 
     // we chose a wrong possible answer, erase it
     eraseAnswer(slotIndex);
   }
+
+  if (numSkips < 7) {
+    slots[slotIndex].answer = "*";
+    if (solve(board, ++numSkips)) {
+      return true;
+    }
+    eraseAnswer(slotIndex);
+  }
   return false;
 }
 
-board = [];
-for (var i = 0; i < 3; i++) 
-	board[i] = [];
+function printBoard(board) {
+  for (var i = 0; i < 15; i++) {
+    cur = ""
+    for (var j = 0; j < 15; j++) {
+      if (board[i][j] == null)
+        cur += "_ ";
+      else
+        cur += (board[i][j] + " ");
+    }
+    console.log(cur);
+  }
+  console.log();
+}
 
-solve(board);
+board = [];
+for (var i = 0; i < 15; i++) {
+	board[i] = [];
+  for (var j = 0; j < 15; j++)
+    board[i][j] = null
+}
+
+for (var i = 0; i < slots.length; i++) {
+  if (!isEmpty(slots[i].answer, false))
+    continue;
+  slot = slots[i];
+  for (var j = 0; j < slot.length; j++) {
+    var curX = slot.startx;
+    var curY = slot.starty;
+    if (slot.orientation == "across")
+      curX += j;
+    else
+      curY += j;
+    board[curY][curX] = slot.answer[j];
+  }
+}
+
+solve(board, 0);
+console.log(maxBoard);
+printBoard(maxBoard);
 
